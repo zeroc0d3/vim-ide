8c28c064a8e2f79880a2a0d2f3508851ecb87a5c
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2016-05-26 11:36:31 +0700

    Fixing Superuser Copy For Step-03

--- step03.sh
+++ step03.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git clone https://github.com/zenorocha/dracula-theme.git
-cp dracula-theme/vim/colors/dracula.vim ~/.vim/bundle/vim-colors/colors/dracula.vim
+sudo cp dracula-theme/vim/colors/dracula.vim ~/.vim/bundle/vim-colors/colors/dracula.vim
 
 git clone https://github.com/blueshirts/darcula.git
-cp darcula/colors/darcula.vim ~/.vim/bundle/vim-colors/colors/darcula.vim 
+sudo cp darcula/colors/darcula.vim ~/.vim/bundle/vim-colors/colors/darcula.vim 
 
-cp 01_vimrc.local.txt > ~/.vimrc.local 
-cp 02_gvimrc.local.txt > ~/.gvimrc.local
+sudo cp 01_vimrc.local.txt > ~/.vimrc.local 
+sudo cp 02_gvimrc.local.txt > ~/.gvimrc.local
