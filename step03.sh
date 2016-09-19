cfe944872d9d742a57dd8d3e99ef70e2dcd8c303
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2016-09-19 15:31:36 +0700

    Update step03.sh
    fixing copy path file

--- step03.sh
+++ step03.sh
@@ -6,5 +6,5 @@ sudo cp dracula-theme/vim/colors/dracula.vim ~/.vim/bundle/vim-colors/colors/dra
 git clone https://github.com/blueshirts/darcula.git
 sudo cp darcula/colors/darcula.vim ~/.vim/bundle/vim-colors/colors/darcula.vim 
 
-sudo cp 01_vimrc.local.txt > ~/.vimrc.local 
-sudo cp 02_gvimrc.local.txt > ~/.gvimrc.local
+sudo cp 01_vimrc.local.txt ~/.vimrc.local 
+sudo cp 02_gvimrc.local.txt ~/.gvimrc.local
