a9c19e2fe92e1a82f548487c82aac0d92dc992f2
Dwi Fahni Denni <dfdenni0912@gmail.com>
2016-09-19 15:56:59 +0700

    Add step04 for default setup vim as editor

---
+++ step04.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
+
+
